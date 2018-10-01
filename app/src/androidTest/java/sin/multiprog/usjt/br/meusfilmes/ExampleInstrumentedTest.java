60ebe50f9cbe27354c9cd1b65d48a3241d00b0f8
paulahasu <paulaakemi14@gmail.com>
2018-09-30 23:17:28 -0300

    CadastraFilmes

---
+++ app/src/androidTest/java/sin/multiprog/usjt/br/meusfilmes/ExampleInstrumentedTest.java
@@ -0,0 +1,31 @@
+package sin.multiprog.usjt.br.meusfilmes;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+/**
+ *
+ *Nome: Paula Akemi Hasu RA817126817
+ *
+ */
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("sin.multiprog.usjt.br.meusfilmes", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/sin/multiprog/usjt/br/meusfilmes/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+package sin.multiprog.usjt.br.meusfilmes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ *
+ *Nome: Paula Akemi Hasu RA817126817
+ *
+ */
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
